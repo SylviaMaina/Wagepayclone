ab166d11d11c1a867d172e47e5f3d1b301f8d88d
Sylvia <sylviamaina16@gmail.com>
2024-07-10 08:49:56 +0300

    with backend

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,38 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+const allowedOrigins = [
+  "https://wagepayclone-epz7.vercel.app",
+  "http://localhost:9000",
+];
 
 app.use(
   cors({
-    origin: "https://wagepayclone-epz7.vercel.app",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
 
+// Middleware
 app.use(bodyParser.json());
 
 // Routes
 app.use("/auth", authRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
